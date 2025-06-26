10ea157ed77c0902ede4411b1d7d24c383c31339
gaurav0973 <codejourneypanda@gmail.com>
2025-06-26 11:01:12 +0530

    database is being connceted

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
 import express from "express";  
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
 
+dotenv.config()
 const app = express();
+
 const port = 3000;
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
-})
+// app.use("/", adminAuth, (req, res) => {
+//     res.send("Hello, Admin!");
+// });
+
+
+
+// db connection
+connectDB()
+    .then(()=> {
+        console.log("Database connected successfully");
+        app.listen(port, ()=>{
+        console.log(`Server is running on port ${port}`);
+        })
+    })
+    .catch((error)=>{
+        console.log("Error while connceting database : ", error.message)
+
+    })
