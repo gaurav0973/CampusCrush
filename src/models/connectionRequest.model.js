2d4a2d42d8573d149b3f78a9ccd0654b85c482e3
gaurav0973 <codejourneypanda@gmail.com>
2025-06-27 22:09:42 +0530

    conncetion request and request route apis created

---
+++ src/models/connectionRequest.model.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const connectionRequestSchema = new mongoose.Schema({
+    fromUserId : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true,
+    },
+    toUserId : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true,
+    },
+    status : {
+        type : String,
+        enum : {
+            values : ["ignored", "interested",  "accepted", "rejected"],
+            message : "Status must be one of the following: ignored, interested, accepted, rejected" 
+        },
+        required : true
+        
+    },
+
+}, {
+    timestamps : true
+})
+
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 })
+
+connectionRequestSchema.pre("save", function(next){
+    const connectionRequest = this
+
+    // check if fromUserId and toUserId are not the same
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error("You cannot send a connection request to yourself");
+    }
+    next()
+})
+
+
+const ConnectionRequestModel =  mongoose.model("ConnectionRequest", connectionRequestSchema);
+export default ConnectionRequestModel;
+
--- src/routes/request.routes.js
+++ src/routes/request.routes.js
@@ -1,12 +1,61 @@
 import express from 'express';
+import { userAuth } from '../middleware/auth.middleware';
+import ConnectionRequest from '../models/connectionRequest.model';
+import User from '../models/user.model';
 
 
 const requestRouter = express.Router();
 
 
-requestRouter.post("/sendConnectionRequest", async (req, res) => {
-    
-})
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
+    try {
+
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status
+
+        // only two statuses are allowed: "ignored" and "interested"
+        const allowedStatus = ["ignored", "interested"]
+        if(!allowedStatus.includes(status)) {
+            return res.status(400).send("Invalid status. Allowed values are: " + allowedStatus.join(", "));
+        }
+
+        // check toUserId is existing user 
+        const toUser = await User.findById(toUserId);
+        if (!toUser){
+            res.json({
+                message: "User with the given ID does not exist",
+                data: null
+            })
+        }
+
+        // Check if a connection request already exists between the two users
+        const existingRequest = await ConnectionRequest.findOne({
+            $or: [
+                { fromUserId: fromUserId, toUserId: toUserId },
+                { fromUserId: toUserId, toUserId: fromUserId }
+            ]
+        });
+        if (existingRequest) {
+            return res.status(400).send("Connection request already exists");
+        }
+
+        const connectionRequest = new ConnectionRequest({
+            fromUserId: fromUserId,
+            toUserId: toUserId,
+            status: status
+        })
+
+        const data = await connectionRequest.save();
+        res.json({
+            message: req.user.firstName + "is " + status + "in "  + toUser.firstName,
+            data: data
+        })
+        
+    } catch (error) {
+        res.status(400).send("Error while sending request: " + error.message);
+    }
+})  
 
 
 
